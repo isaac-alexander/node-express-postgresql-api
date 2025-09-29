18196d5e17e7869276d7daf203b5f35a02efcd26
Isaac Alexander <Isaacalexander81@gmail.com>
2025-09-29 02:11:21 +0100

    middleware/authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,29 +1,48 @@
 require('dotenv').config();
-const jwt = require('../libraries/JWT')
-const db = require('../libraries/Database')
+const jwt = require('../libraries/JWT');
+const db = require('../libraries/Database');
 
 module.exports = async (req, res, next) => {
     let token = req.headers.authorization || req.headers.Authorization;
-    token = token.split(" ")[1]
-    try {
-        let { userId } = await jwt.verifyToken(token)
-        const user = await db.query(`SELECT email from users where userid =$1`, [userId])
-        if (user.rowCount > 0) {
-            req.userId = userId
-            return next()
-        }
-        return res.status(400).json({
-            "status": "Bad Request",
-            "message": "Client error",
-            "statusCode": 400
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json({
-            "status": "Bad Request",
-            "message": "Client error",
-            "statusCode": 400
-        })
+
+    if (!token) {
+        return res.status(401).json({
+            "status": "Unauthorized",
+            "message": "No token provided",
+            "statusCode": 401
+        });
     }
 
-}
+    // Expecting "Bearer <token>"
+    token = token.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({
+            "status": "Unauthorized",
+            "message": "Malformed token",
+            "statusCode": 401
+        });
+    }
+
+    try {
+        let { userId } = await jwt.verifyToken(token);
+        const user = await db.query(`SELECT email FROM users WHERE userid = $1`, [userId]);
+
+        if (user.rowCount > 0) {
+            req.userId = userId;
+            return next();
+        }
+
+        return res.status(400).json({
+            "status": "Bad Request",
+            "message": "User not found",
+            "statusCode": 400
+        });
+    } catch (error) {
+        console.error("JWT Error:", error);
+        return res.status(401).json({
+            "status": "Unauthorized",
+            "message": "Invalid or expired token",
+            "statusCode": 401
+        });
+    }
+};
